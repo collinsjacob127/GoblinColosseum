f5e31aedcef2c86994198c8d4e5a88c90a1511ae
collinsjacob127 <collinsjacob127@gmail.com>
2025-09-02 00:59:58 -0700

    Switching to SDL3

---
+++ game/src/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Author: Jacob Collins
+ * Description: Main file for Goblin Colosseum
+ *  Handles initialization and cleanup of game.
+ */
+ 
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
