2af178403733a69981cfda1b15abbb24714592d4
collinsjacob127 <collinsjacob127@gmail.com>
2025-11-05 13:18:33 -0800

    Confirmed no memory leaks in server or client

---
+++ server/database.hpp
@@ -0,0 +1,10 @@
+/**
+ * Author: Jacob Collins
+ * Date: 11/5/2025
+ * Description:
+ * Headers for server data structures.
+ * Lobbies: Instanced mapping of session IDs to PlayerEntities for matchmaking.
+ * Registry: Persistent saved mapping of unique player IDs to PlayerEntities.
+ * LeaderBoard: History of wins/losses by Player and Character.
+ */
+
