de9c8ed6f4792ff5d8b66865c2909520643a24ff
collinsjacob127 <collinsjacob127@gmail.com>
2025-12-16 20:32:06 -0800

    Update server address

--- game/src/net/net.hpp
+++ game/src/net/net.hpp
@@ -74,7 +74,8 @@ typedef SSIZE_T ssize_t;
 #define ENABLE_DENSE_PACKET_INSPECTION true
 
 // IPV4 addr of the server
-constexpr char *SERVER_ADDR = (char*)"192.168.1.100"; // Server PC
+// constexpr char *SERVER_ADDR = (char*)"192.168.1.100"; // Server PC
+constexpr char *SERVER_ADDR = (char*)"35.227.175.118"; // Server PC
 // constexpr char *SERVER_ADDR = (char*)"192.168.1.133"; // Desktop - public
 // constexpr char *SERVER_ADDR = (char*)"172.25.116.169"; // Desktop - private
 // constexpr char *SERVER_ADDR = (char*)"127.0.0.1";
