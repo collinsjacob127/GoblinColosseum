03a66c3e28e3ad37a662913b96c5a7ebef317e32
collinsjacob127 <collinsjacob127@gmail.com>
2025-12-17 21:57:44 -0800

    Update - reduced fps

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -18,7 +18,7 @@
 #include "render/render.hpp"
 #include "characters/characters.hpp"
 
-#define FRAME_RATE_CAP 60
+#define FRAME_RATE_CAP 45
 
 // Skeleton of SDL basic calls provided by
 // [glusoft](https://glusoft.com/sdl3-tutorials/install-sdl3-linux-cmake/)
